f7c22dca045801c905842d118491165c733671b9
Anna <nianna358@gmail.com>
2026-09-19 14:55:17 +0700

    Движение по оси Oy и по Ox.

--- main.c
+++ main.c
@@ -8,8 +8,9 @@ int sqr(int x)
     return x * x;
 }
 
-void square(Frame *frame, int *per);
-
+void square(Frame *frame, int *per,int x,int y);
+void moveXY(int  *const p_x,int  * const p_is_right,int znach);
+void moveY(int  *const p_y,int  * const p_is_Up,int znach);
 int main()
 
 {
@@ -24,7 +25,12 @@ int main()
 
     drawBackground(frame, blackF);
     display(frame);
+    int x=12;
+    int y=7;
     int per = 0;
+    int is_right=1;
+    int is_Up=1;
+    
     while (1)
     {
         if (GetAsyncKeyState(VK_ESCAPE))
@@ -34,8 +40,21 @@ int main()
 
         drawBackground(frame, blackF);
 
-        square(frame, &per);
+        square(frame, &per,x,y);
         display(frame);
+        moveXY(&x,&is_right,40);
+        Sleep(100);
+        moveXY(&y,&is_Up,40);
+
+
+
+        
+
+    // if (GetAsyncKeyState(VK_LEFT))
+    // {
+    //     *per -= 1;
+    //     Sleep(100);
+    // }
     }
 
     make2DConsole(frame, 10, 16, L"Your title");
@@ -43,6 +62,32 @@ int main()
     frameDeinit(frame);
     free(frame);
 }
+
+
+void moveXY(int  *const p_xy,int  * const p_is_forward,int border)
+{
+    if ((*p_xy)!=border && (*p_is_forward)==1)
+    {   
+        (*p_xy)+=1;
+        
+    }
+    if (*p_xy==border)
+    {
+        (*p_is_forward)=0;
+    }
+    if ((*p_xy)!=1 && (*p_is_forward)==0)
+    {   
+        (*p_xy)-=1;
+        
+    }
+    if (*p_xy==1)
+    {
+        (*p_is_forward)=1;
+    }
+}
+
+
+
 
 void line(Frame *frame,int x,int y,int x1,int y1)
 {
@@ -63,31 +108,20 @@ void line(Frame *frame,int x,int y,int x1,int y1)
         }
     }
 }
-void square(Frame *frame, int *per)
-{
-    int x = 12;
-    int y = 7;
 
+void square(Frame *frame, int *per,int x,int y)
+{
+    
     frame->penColor = yellowF;
     frame->penWchar = 0x2588;
     wchar_t debugStr[256];
     swprintf_s(debugStr, 256, L" %d %d %d", x, y, *per);
     drawText(frame, 0, 0, debugStr, wcslen(debugStr), whiteF);
 
-    line(frame, 12+ *per,7,46+ *per,7);//X
-    line(frame, 12+ *per,7,12+ *per,46);//Y
-    line(frame, 12+ *per,46,46+ *per,46);//X
-    line(frame, 46+ *per,7,46+ *per,47);
-    
-    if (GetAsyncKeyState(VK_RIGHT))
-    {
-        *per += 1;
-        Sleep(100);
-    }
-    if (GetAsyncKeyState(VK_LEFT))
-    {
-        *per -= 1;
-        Sleep(100);
-    }
-
+    line(frame, x+ *per,y,x+34+ *per,y);//X
+    line(frame, x+ *per,y,x+ *per,y+34);//Y
+    line(frame, x+ *per,y+34,x+34+ *per,y+34);//X
+    line(frame, x+34+ *per,y,x+34+ *per,y+35);
 }
+
+
